62d8abc352dfca06bc4cff4cd9dbf320ae5dd88c
Ramakrishna <ramakchqa@gmail.com>
2019-09-07 13:29:42 +0530

    this is my First commit

---
+++ src/test/java/MyMaven/MavenProject/FirstTest.java
@@ -0,0 +1,18 @@
+package MyMaven.MavenProject;
+
+import org.testng.annotations.Test;
+
+public class FirstTest 
+{
+
+	@Test
+	public void LoginThirdTest()
+	{
+		System.out.println("My Third Test Login");
+	}
+	@Test
+	public void LogoutThirdTest()
+	{
+		System.out.println("My Third Test Logout");
+	}
+}
---
+++ src/test/java/MyMaven/MavenProject/ThirdTest.java
@@ -0,0 +1,18 @@
+package MyMaven.MavenProject;
+
+import org.testng.annotations.Test;
+
+public class ThirdTest 
+{
+
+	@Test
+	public void LoginFirstTest()
+	{
+		System.out.println("My First Test Login");
+	}
+	@Test
+	public void LogoutFirstTest()
+	{
+		System.out.println("My First Test Logout");
+	}
+}
